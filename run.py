ab59708fe1c859e7987d0f1f640f62d0c1fb7c38
Armin <arminonline71@gmail.com>
2024-07-08 01:35:24 +0330

    Update

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-#https://github.com/jakegrigsby/deep_control/
+# https://github.com/jakegrigsby/deep_control/
 # https://arxiv.org/pdf/2006.15134
 
 from awac_rl import AWAC
@@ -7,9 +7,12 @@ from armin_RL.envs import gym
 
 
 cfg = utils.yaml_to_dict("C:/Users/armin/Documents/GitHub/awac_rl/cfg.yaml")
-env = gym.Make('MountainCarContinuous-v0')
 
-awac = AWAC.AWAC(env=env, cfg=cfg)
-awac.run()
 
+total_steps = cfg['num_steps_offline'] + cfg['num_steps_online']
+done = True
+
+for step in range(total_steps):
+    if step > cfg['num_steps_offline']:
+        for _ in range(cfg['transitions_per_online_step']):
 
