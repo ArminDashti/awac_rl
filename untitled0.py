9cb5a56593369f62b98ca44fec6e367a0ca8a97c
Armin <arminonline71@gmail.com>
2024-07-11 21:16:48 +0330

    Update

--- untitled0.py
+++ untitled0.py
@@ -1,12 +1,59 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jul  2 22:35:52 2024
+import torch
+from torch.distributions import Distribution, Normal
+import rlkit.torch.pytorch_util as ptu
 
-@author: armin
-"""
 
-import gymnasium as gym
-env = gym.make('Ant-v4')
-#%%
-import gymnasium as gym
-env = gym.make('HalfCheetah-v2')
+
+class TanhNormal(Distribution):
+    def __init__(self, normal_mean, normal_std, epsilon=1e-6):
+        self.normal_mean = normal_mean
+        self.normal_std = normal_std
+        self.normal = Normal(normal_mean, normal_std)
+        self.epsilon = epsilon
+
+
+    def sample_n(self, n, return_pre_tanh_value=False):
+        z = self.normal.sample_n(n)
+        if return_pre_tanh_value:
+            return torch.tanh(z), z
+        else:
+            return torch.tanh(z)
+
+
+    def log_prob(self, value, pre_tanh_value=None):
+        if pre_tanh_value is None:
+            pre_tanh_value = torch.log(
+                (1+value) / (1-value)
+            ) / 2
+        return self.normal.log_prob(pre_tanh_value) - torch.log(
+            1 - value * value + self.epsilon
+        )
+
+
+    def sample(self, return_pretanh_value=False):
+        """
+        Gradients will and should *not* pass through this operation.
+        See https://github.com/pytorch/pytorch/issues/4620 for discussion.
+        """
+        z = self.normal.sample().detach()
+
+        if return_pretanh_value:
+            return torch.tanh(z), z
+        else:
+            return torch.tanh(z)
+
+
+        z = (
+            self.normal_mean +
+            self.normal_std *
+            Normal(
+                ptu.zeros(self.normal_mean.size()),
+                ptu.ones(self.normal_std.size())
+            ).sample()
+        )
+        z.requires_grad_()
+
+        if return_pretanh_value:
+            return torch.tanh(z), z
+        else:
+            return torch.tanh(z)
